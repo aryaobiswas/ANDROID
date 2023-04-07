6760dbc293402de362fa280ae0ddf0cd89425f5c
Arya <redarya1234@gmail.com>
2023-04-07 12:42:48 +0530

    FITNESS APP

---
+++ FitNest/app/src/main/java/com/example/fitnest/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.fitnest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void beforeage18(View view) {
+    }
+
+    public void afterage18(View view) {
+    }
+
+    public void food(View view) {
+    }
+}
